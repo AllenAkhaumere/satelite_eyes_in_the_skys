817b505727473550d266e8ebe39289c8757d4927
Allen Akhaumere <allenakhaumere@gmail.com>
2021-03-01 14:44:58 +0100

    updated

--- classification/eurosat/pytorch lightning/model.py
+++ classification/eurosat/pytorch lightning/model.py
@@ -29,11 +29,11 @@ def SatResNet50(num_classes, pretrained):
     return model
 
 class LitResnet(pl.LightningModule):
-    def __init__(self, lr=0.05):
+    def __init__(self, lr=0.05, num_classes):
         super().__init__()
 
         self.save_hyperparameters()
-        self.model = create_model()
+        self.model = SatResNet50(num_classes, pretrained=True)
 
     def forward(self, x):
         out = self.model(x)
@@ -52,3 +52,21 @@ class LitResnet(pl.LightningModule):
         loss = F.nll_loss(logits, y)
         preds = torch.argmax(logits, dim=1)
         acc = accuracy(preds, y)
+         if stage:
+            self.log(f'{stage}_loss', loss, prog_bar=True)
+            self.log(f'{stage}_acc', acc, prog_bar=True)
+
+    def validation_step(self, batch, batch_idx):
+        self.evaluate(batch, 'val')
+
+    def test_step(self, batch, batch_idx):
+        self.evaluate(batch, 'test')
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.parameters(), lr=self.hparams.lr, momentum=0.9, weight_decay=5e-4)
+        steps_per_epoch = 45000 // batch_size
+        scheduler_dict = {
+            'scheduler': OneCycleLR(optimizer, 0.1, epochs=self.trainer.max_epochs, steps_per_epoch=steps_per_epoch),
+            'interval': 'step',
+        }
+        return {'optimizer': optimizer, 'lr_scheduler': scheduler_dict}
